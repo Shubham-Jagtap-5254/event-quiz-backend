d7d5991d6518c014ffdc487eb4015d3a34b5b845
Shubham Jagtap <shubhamjagtap071104@gmail.com>
2026-04-08 22:36:48 +0530

    corrs done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Lead = require('./models/Lead');
 
 const app = express();
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://event-quiz-frontend.onrender.com'], // Explicitly allow frontend origins
+  origin: ['http://localhost:5173', 'https://event-quiz-backend.onrender.com'], // Explicitly allow frontend origins
   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
